72c34cc694c2c9542fd55fa9885df78b9143570c
AlejandroFontesAlbeza <alexfontesalbeza@gmail.com>
2026-03-16 19:24:23 +0100

    add config.py from refactoring branch for github actions, testing

---
+++ config.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).resolve().parent
+
+
+
+# Define dataset paths
+DATA_DIR = ROOT_DIR / "data"
+
+# Paths UE dataset -> Preprocessed dataset; pre_processed_dataset.bat
+
+UE_IMAGES_DIR = DATA_DIR / "renderUE" / "images"
+UE_MASKS_DIR = DATA_DIR / "renderUE" / "masks"
+PREPROCESSED_IMAGES_DIR = DATA_DIR / "datasetPreProcessed" / "images"
+PREPROCESSED_MASKS_DIR = DATA_DIR / "datasetPreProcessed" / "masks"
+
+
+# Paths dataset Structured -> train & valid; dataset_splitter.bat
+DATASET_IMAGES_TRAIN_DIR = DATA_DIR / "dataset" / "train" / "images"
+DATASET_MASKS_TRAIN_DIR = DATA_DIR / "dataset" / "train" / "masks"
+DATASET_IMAGES_VALID_DIR = DATA_DIR / "dataset" / "valid" / "images"
+DATASET_MASKS_VALID_DIR = DATA_DIR / "dataset" / "valid" / "masks"
+
+
+#Define model paths
+MODELS_DIR = ROOT_DIR / "models"
+MODEL_PATH = MODELS_DIR / "unet_modelV0.pth"
+NEW_MODEL_PATH = MODELS_DIR / "unet_modelNew.pth"
+
+
+
+
+
+### TESTING PATHS
+
+TEST_DATASET_IMAGES_TRAIN_DIR = DATA_DIR / "test_dataset" / "train" / "images"
+TEST_DATASET_MASKS_TRAIN_DIR = DATA_DIR / "test_dataset" / "train" / "masks"
+TEST_DATASET_IMAGES_VALID_DIR = DATA_DIR / "test_dataset" / "valid" / "images"
+TEST_DATASET_MASKS_VALID_DIR = DATA_DIR / "test_dataset" / "valid" / "masks"
